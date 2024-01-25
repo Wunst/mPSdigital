a9c1b93d9e01ce128692cc75f2c64590ab888c81
Wunst <matthiesbe@gmail.com>
2024-01-25 09:13:45 +0100

    Add data source

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,15 @@
 import { createServer } from 'http';
+import { AppDataSource } from './data-source';
 
 const port = 3001;
+
+AppDataSource.initialize()
+    .then(() => {
+        console.log("Data Source has been initialized!");
+    })
+    .catch((err) => {
+        console.error("Error during Data Source initialization", err);
+    });
 
 const server = createServer((req, res) => {
     res.statusCode = 200;
---
+++ src/data-source.ts
@@ -0,0 +1,9 @@
+import { DataSource } from 'typeorm';
+
+export const AppDataSource = new DataSource({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'mpsdigital',
+    database: 'mpsdigital'
+});

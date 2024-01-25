b31c565826383e73a17d5f67182243598946a0c3
Wunst <matthiesbe@gmail.com>
2024-01-25 10:38:55 +0100

    Add User table

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,9 +1,13 @@
 import { DataSource } from 'typeorm';
+import { User } from './user';
 
 export const AppDataSource = new DataSource({
     type: 'postgres',
     host: 'localhost',
     port: 5432,
     username: 'mpsdigital',
-    database: 'mpsdigital'
+    database: 'mpsdigital',
+    entities: [
+        User
+    ]
 });

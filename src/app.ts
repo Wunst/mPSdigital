4553c1ac6bea8fcd1b8b83391d842d46f527fb9b
Wunst <matthiesbe@gmail.com>
2024-01-26 10:22:40 +0100

    Use new ExpressJS server for login entry point

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,32 @@
-import { createServer } from 'http';
+import express from 'express';
 import { AppDataSource } from './data-source';
 import { User } from './user';
 
 const port = 3001;
 
+const app = express();
+
+app.get('/login', async (req, res) => {
+    const user = await User.findOneBy({ username: req.body['username'] });
+    if (!user) {
+        res.statusCode = 401;
+        res.end();
+    }
+
+    if (user?.password != req.body['password']) {
+        res.statusCode = 401;
+        res.end();
+    }
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/json');
+});
+
 AppDataSource.initialize()
     .then(() => {
         console.log("Data Source has been initialized!");
-        User.find()
-            .then(users => {
-                console.log(users);
-            });
+        app.listen(port, () => console.log("Server listening on port", port));
     })
     .catch((err) => {
         console.error("Error during Data Source initialization", err);
     });
-
-const server = createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, world!');
-});
-
-server.listen(port);

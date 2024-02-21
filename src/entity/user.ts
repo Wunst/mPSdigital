c1140a503f0e12b8a75efcb40d15a0400891e794
Wunst <matthiesbe@gmail.com>
2024-02-21 14:13:37 +0100

    Add dummy value for `allForms`

--- src/entity/user.ts
+++ src/entity/user.ts
@@ -203,7 +203,8 @@ async function createUser(req: express.Request, res: express.Response) {
     await User.insert({
         username: req.body['username'],
         password: await hashPassword(req.body['username']),
-        role: req.body['role']
+        role: req.body['role'],
+        allForms: true // TODO
     });
 
     if (req.body['role'] === Role.student) {

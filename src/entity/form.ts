261beece5c3db2f31ae27c64c747d26298e0ca4f
Colbert22 <finnja.behrend@mps-ki.de>
2024-05-20 14:50:36 +0200

    add admins

--- src/entity/form.ts
+++ src/entity/form.ts
@@ -32,7 +32,7 @@ export async function create(req: express.Request, res: express.Response) {
         return;
     }
 
-    if(loggedInUser.role !== Role.teacher){
+    if(loggedInUser.role === Role.teacher){
         res.status(403).end();
         return;
     }

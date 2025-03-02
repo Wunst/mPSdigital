0843c13eac062eca86c77e4458cc21fc3523a51e
Colbert22 <finnja.behrend@mps-ki.de>
2025-03-02 22:57:00 +0100

    Excursion and special consent utils

--- src/utils/groupUtils.ts
+++ src/utils/groupUtils.ts
@@ -1,12 +1,34 @@
 import { IsNull, MoreThan, Or } from "typeorm";
 import { Group } from "../entity/group";
 import { User } from "../entity/user";
+import { SpecialParentalConsent } from "../entity/specialParentalConsent";
+import { Excursion, Status } from "../entity/excursion";
 
-export async function groupGetCurrent(user: User) {
+export async function groupGetCurrent(user: User): Promise<Group | null> {
     return Group.findOneBy({
         student: {
             user
         },
         endDate: Or(MoreThan(new Date()), IsNull())
+    })
+}
+
+export async function groupGetSpecialConsent(group: Group | null, user: User): Promise<boolean> {
+    return !!group && !!await SpecialParentalConsent.findOneBy({
+        student: {
+            user
+        },
+        group
+    })
+}
+
+/**
+ * Checks whether the group has an *accepted* excursion on some day.
+ */
+export async function groupHasExcursion(group: Group | null, date: Date): Promise<boolean> {
+    return !!group && !!await Excursion.findOneBy({
+        group,
+        date,
+        status: Status.accepted
     })
 }

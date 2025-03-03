e0398b0120ed1ff983cf0d663fe3c91be70aa610
Colbert22 <finnja.behrend@mps-ki.de>
2025-03-03 14:26:03 +0100

    Role utils

--- src/utils/roleUtils.ts
+++ src/utils/roleUtils.ts
@@ -7,3 +7,10 @@ import { Role } from "../entity/user";
 export function roleCanTarget(me: Role, you: Role): boolean {
    return me == Role.admin || me == Role.teacher && you == Role.student
 }
+
+/**
+ * Returns true if I am a teacher or admin.
+ */
+export function roleIsTeacher(me: Role): boolean {
+    return me == Role.admin || me == Role.teacher
+}
